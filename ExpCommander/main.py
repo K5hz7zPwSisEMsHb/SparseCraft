5136258ab5a907b3a0ee4868440b0f8602c3cde8
zxnvsvsbpxevmuxa <crarywahabyt@outlook.com>
2025-04-26 18:44:52 +0800

    first commit

--- ExpCommander/main.py
+++ ExpCommander/main.py
@@ -227,6 +227,7 @@ def fine_tune_new():
         if not os.path.exists(f'{rt_dir}/SparseCraft/SlimeNet/model/{platform}/spmm_fine_tune.bin'):
             QproDefaultConsole.print("Start generating SpMM fine-tuning data")
             os.chdir(f"{rt_dir}/SparseCraft/DataGen")
+            os.makedirs('gen/spmm', exist_ok=True)
             os.system(f'qrun run spmm --device {device} --batch {rt_dir}/SparseCraft/DataSet/WheatFarm-T.txt')
             os.chdir("gen/spmm")
             os.system(f"mv *.txt {platform}")
@@ -245,6 +246,7 @@ def fine_tune_new():
         if not os.path.exists(f'{rt_dir}/SparseCraft/SlimeNet/model/{platform}/spmv_fine_tune.bin'):
             QproDefaultConsole.print("Start generating SpMV fine-tuning data")
             os.chdir(f"{rt_dir}/SparseCraft/DataGen")
+            os.makedirs('gen/spmv', exist_ok=True)
             os.system(f'qrun run spmv --device {device} --batch {rt_dir}/SparseCraft/DataSet/WheatFarm-T.txt')
             os.chdir("gen/spmv")
             os.system(f"mv *.txt {platform}")
